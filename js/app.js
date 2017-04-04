f4eaf1f3a74bb6efe59124461a3d6e6a75cc6e3e
ramesh kumar <ramesh kumar>
2017-04-04 15:09:17 -0500

    adds new contact

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("myController", function($scope) {
     console.log("In Controller...");
-
+    $scope.newUser = {};
 
     $scope.contacts = [
         {FirstName: "Ramesh", LastName: "Kumar", Email: "abc@gmail.com", PhoneNumber: "9999999999", Status: "Active"},
         {FirstName: "Ramesh", LastName: "Kumar", Email: "abc@gmail.com", PhoneNumber: "9999999999", Status: "Active"},
         {FirstName: "Ramesh", LastName: "Kumar", Email: "abc@gmail.com", PhoneNumber: "9999999999", Status: "Active"}
     ];
+
+    $scope.saveUser = function() {
+        $scope.contacts.push($scope.newUser);
+        console.log($scope.contacts);
+        console.log($scope.newUser);
+        $scope.newUser = {};
+    };
 });
